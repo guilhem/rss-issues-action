1bbdc6f37c1121886d2aea8a5142bcf4582f5ab3
Guilhem Lettron <guilhem@barpilot.io>
2020-05-19 01:48:30 +0200

    Fix item date management
    Use a fork of gofeed with stable published time
    Filter before loop

--- go.mod
+++ go.mod
@@ -10,3 +10,5 @@ require (
 	github.com/thoas/go-funk v0.6.0
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 )
+
+replace github.com/mmcdole/gofeed => github.com/guilhem/gofeed v1.0.1-0.20200518231152-db85dfc4ee46
--- main.go
+++ main.go
@@ -21,38 +21,12 @@ import (
 )
 
 func main() {
-
 	ghaLogOption := &gha.LogOption{File: "main.go"}
 
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: gha.GetInput("repo-token")},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
-
-	fp := gofeed.NewParser()
-
-	feed, err := fp.ParseURL(gha.GetInput("feed"))
-	if err != nil {
-		gha.Error(fmt.Sprintf("Cannot parse feed '%s': '%s'", gha.GetInput("feed"), err), ghaLogOption)
-		os.Exit(1)
-	}
-	gha.Info(feed.Title)
-
-	IssueListByRepoOption := &github.IssueListByRepoOptions{}
+	// Parse repository in form owner/name
 	repo := strings.Split(os.Getenv("GITHUB_REPOSITORY"), "/")
-	issues, _, err := client.Issues.ListByRepo(ctx, repo[0], repo[1], IssueListByRepoOption)
-	if err != nil {
-		gha.Error(fmt.Sprint(err), ghaLogOption)
-		os.Exit(1)
-	}
-	gha.Debug(fmt.Sprintf("Issues %v", issues), ghaLogOption)
 
-	converter := md.NewConverter("", true, nil)
-
-	var createdIssues []string
+	// Parse limit time option
 	var limitTime time.Time
 	if d, err := time.ParseDuration(gha.GetInput("lastTime")); err == nil {
 		// Make duration negative
@@ -65,29 +39,55 @@ func main() {
 	}
 	gha.Debug(fmt.Sprintf("limitTime %s", limitTime), ghaLogOption)
 
+	// Parse Labels
 	labels := strings.Split(gha.GetInput("labels"), ",")
 	gha.Debug(fmt.Sprintf("labels %v", labels), ghaLogOption)
+
+	ctx := context.Background()
+
+	// Instanciate GitHub client
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: gha.GetInput("repo-token")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	// Instanciate feed parser
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURLWithContext(gha.GetInput("feed"), ctx)
+	if err != nil {
+		gha.Error(fmt.Sprintf("Cannot parse feed '%s': '%s'", gha.GetInput("feed"), err), ghaLogOption)
+		os.Exit(1)
+	}
+	gha.Info(feed.Title)
+
+	// Instanciate HTML to markdown
+	converter := md.NewConverter("", true, nil)
+
+	// Remove old items in feed
+	feed.Items = funk.Filter(feed.Items, func(x *gofeed.Item) bool {
+		return x.PublishedParsed.After(limitTime)
+	}).([]*gofeed.Item)
+
+	// Get all issues
+	IssueListByRepoOption := &github.IssueListByRepoOptions{
+		State:  "all",
+		Labels: labels,
+	}
+
+	issues, _, err := client.Issues.ListByRepo(ctx, repo[0], repo[1], IssueListByRepoOption)
+	if err != nil {
+		gha.Error(fmt.Sprint(err), ghaLogOption)
+		os.Exit(1)
+	}
+	gha.Debug(fmt.Sprintf("%d issues", len(issues)), ghaLogOption)
+
+	var createdIssues []string
 
 	// Iterate
 	for _, item := range feed.Items {
 		title := strings.Join([]string{gha.GetInput("prefix"), item.Title}, " ")
 		gha.Debug(fmt.Sprintf("Issue '%s'", title), ghaLogOption)
-
-		var t *time.Time
-		if item.UpdatedParsed != nil {
-			t = item.UpdatedParsed
-			gha.Debug("Use 'updated' field", ghaLogOption)
-		} else if item.PublishedParsed != nil {
-			t = item.PublishedParsed
-			gha.Debug("Use 'published' field", ghaLogOption)
-		} else {
-			gha.Warning("No timed field", ghaLogOption)
-		}
-
-		if t != nil && t.Before(limitTime) {
-			gha.Debug(fmt.Sprintf("Item date '%s' is before limit", t), ghaLogOption)
-			continue
-		}
 
 		if issue := funk.Find(issues, func(x *github.Issue) bool {
 			return *x.Title == title
